fa2454b008661e8ff52d30e0436c5b491f5f89fc
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-10-03 11:15:38 +0700

    init: initial commit

---
+++ android/app/src/main/kotlin/com/example/e_begal_shield_rewritten/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.e_begal_shield_rewritten
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,6 @@
-import 'package:e_begal_shield_rewritten/global/config/router.config.dart';
-import 'package:e_begal_shield_rewritten/global/themes/color.theme.dart';
-import 'package:e_begal_shield_rewritten/global/themes/text.theme.dart';
+import 'package:e_begal_shield_rewritten/app/e_begal_shield.app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: MaterialApp.router(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: colorScheme,
-          useMaterial3: true,
-          fontFamily: 'Poppins',
-          textTheme: textTheme,
-        ),
-        routerConfig: routerConfig,
-      ),
-    );
-  }
+  runApp(const EBegalShieldApp());
 }
