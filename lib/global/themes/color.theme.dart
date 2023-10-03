d9e62345268d2975fb4dcb5155c56cec1ded7b19
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-10-03 11:09:29 +0700

    feat: added text and color themes

---
+++ lib/global/themes/color.theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final colorScheme = ColorScheme.fromSeed(
+  seedColor: const Color(0xFF2360A1),
+  secondary: Colors.grey,
+);
---
+++ lib/global/themes/text.theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+const textTheme = TextTheme(
+  titleLarge: TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+  ),
+  labelLarge: TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+  ),
+  labelMedium: TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  ),
+  labelSmall: TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  ),
+  bodyMedium: TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  ),
+);
