91b8797a45a7f26a25c899fef9330c7ddb0ccb02
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-10-03 17:01:39 +0700

    style: refactored root widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,7 @@ import 'package:e_begal_shield_rewritten/app/e_begal_shield.app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const EBegalShieldApp());
+  runApp(
+    const EBegalShieldApp(),
+  );
 }
