83eb142f7d1feaa095514e957cfe4bb4983bb3d6
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-11-13 23:54:58 +0700

    feat: initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:e_begal_shield_rewritten/app/e_begal_shield.app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
-    const EBegalShieldApp(),
+    const ProviderScope(
+      child: EBegalShieldApp(),
+    ),
   );
 }
